1d3521221e6117725507feadd51807b157938666
hwb <hwb.163.com>
2017-08-31 13:03:32 +0800

    完成讲师启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -12,12 +12,12 @@ define(['jquery','template','bootstrap'],function($,template){
      $('#teacherInfo').html(html);
 
      //预览查看功能
-       $('.preview').click(function(){
+     $('.preview').click(function(){
        	//closest  离他最近的
        	//获取当前记录的ID
-   	     var td=$(this).closest('td');
-         var tcId=td.attr('data-tcId');
-         console.log(tcId);
+       	var td=$(this).closest('td');
+       	var tcId=td.attr('data-tcId');
+       	console.log(tcId);
          //根据id查询数据
          $.ajax({
          	type:'get',
@@ -33,11 +33,40 @@ define(['jquery','template','bootstrap'],function($,template){
               $('#modalInfo').html(html);
               //显示弹窗
               $('#teacherModal').modal();
+          }
+      });
+     });
+       //控制启用和注销
+       $('.eod').click(function(){
+       	var td=$(this).closest('td');
+       	var tcId=td.attr('data-tcId'); 
+       	var tcStatus=td.attr('data-status'); 
+       	var that=this;
+       	$.ajax({
+       		type:'post',
+       		url:'/api/teacher/handle',
+       		data:{
+       			tc_id:tcId,
+       			tc_status:tcStatus
+       		},
+       		dataType:'json',
+       		success:function(data){
+       			// console.log(data);
+       			if(data.code==200){
+         		//修改当前的状态
+         		td.attr('data-status',data.result.tc_status);
+         		//修改文字信息
+         		if(data.result.tc_status==0){
+         			$(that).html('注 销');
+         		}else{
+         			$(that).html('启 用');
+         		}
          	}
-         })
+         }
+     })
        })
-   	}
+   }
 
-   })
+})
 
 });
