b179e2a99eea5a4688eb2149183fa4c1d61dbf01
hwb <hwb.163.com>
2017-09-01 11:07:13 +0800

    添加日期和选择插件

--- public/js/config.js
+++ public/js/config.js
@@ -5,19 +5,25 @@ require.config({
 		cookie:'jquery-cookie/jquery.cookie',
 		template:'artTemplate/template-web',
 		bootstrap:'bootstrap/js/bootstrap.min',
+		datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
+		language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
 		common:'../js/common',
 		login:'../js/login',
 		index:'../js/index',
-        teacherlist:'../js/teacher-list',
-        teacheradd:'../js/teacher-add',
-        util:'../js/util'
+		teacherlist:'../js/teacher-list',
+		teacheradd:'../js/teacher-add',
+		util:'../js/util'
 	},
 	//做兼容
 	shim:{
-      bootstrap:{
+		bootstrap:{
       	//依赖
       	deps:['jquery']
-      }
-	}
+      },
+      language:{
+      	//依赖模块
+      	deps:['jquery','datepicker']
+      }  
+  }
 });
 
