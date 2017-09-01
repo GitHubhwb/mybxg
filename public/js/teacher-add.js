8f11a332bc0b9791a211fa90058e08e1099e4d48
hwb <hwb.163.com>
2017-09-01 13:36:31 +0800

    添加表单提交插件功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','datepicker','language','validate'],function($,template,util){
+define(['jquery','template','util','datepicker','language','validate','form'],function($,template,util){
   util.setMenu('/teacher/list');
   //获取编辑的讲师id
   var tcId=util.qs('tc_id');
@@ -31,7 +31,16 @@ define(['jquery','template','util','datepicker','language','validate'],function(
       sendForm:false,
       valid:function(){
         //提交表单
-        
+        $(this).ajaxSubmit({
+          type:'post',
+          url:url,
+          dataType:'json',
+          success:function(data){
+             if(data.code==200){
+              location.href='/teacher/list';
+             }
+          }
+        });
       },
       description:{
         tc_name:{
