91e5144ab0879fda8409619d3a75fe4d9b425e6c
hwb <hwb.163.com>
2017-09-06 11:49:20 +0800

    完成编辑和添加提交功能

--- public/js/courselesson.js
+++ public/js/courselesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util','bootstrap'],function($,template,util){
+define(['jquery','template','util','bootstrap','form'],function($,template,util){
 	util.setMenu('/course/list');
 	//获取课程id
 	var csId=util.qs('cs_id');
@@ -11,19 +11,43 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
     	success:function(data){
     		var html=template('lessonTpl',data.result);
     		$('#lessonInfo').html(html);
+
+    		//表单提交
+    		function submitForm(url,ctId){
+    			var param={ct_cs_id:csId};
+    			if(ctId){
+               	 	param.ct_id=ctId;
+               	}
+               $('#submitBtn').click(function(){
+               	 $('#modalForm').ajaxSubmit({
+                    type:'post',
+                    url:url,
+                    data:param,
+                    dataType:'json',
+                    success:function(data){
+                    	if(data.code==200){
+                    		location.reload();
+                    	}
+                    }
+               	 });
+               });
+    		}
     		//添加课时
     		$('#addBtn').click(function(){
     			var html=template('modalTpl',{operate:'添加课时'});
     		    $('#modalInfo').html(html);
     			$('#chapterModal').modal();
+    			//添加提交表单
+    			submitForm('/api/course/chapter/add');
     		});
     		//处理编辑课时
     		$('.editBtn').click(function(){
     			//查询数据
+    			var ctId=$(this).attr('data-ctId');
     			$.ajax({
     				type:'get',
     				url:'/api/course/chapter/edit',
-    				data:{ct_id:$(this).attr('data-ctId')},
+    				data:{ct_id:ctId},
     				dataType:'json',
     				success:function(data){
     					//渲染模版
@@ -32,9 +56,11 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
     					$('#modalInfo').html(html);
     					//显示弹窗
     					$('#chapterModal').modal();
+    					//编辑课时
+    					submitForm('/api/course/chapter/modify',ctId);
     				}
-    			})
-    		})
+    			});
+    		});
     	}
     });
 });
