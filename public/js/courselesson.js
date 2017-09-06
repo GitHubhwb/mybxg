92935b2b3b7074add3695311a71b414b57b70f99
hwb <hwb.163.com>
2017-09-06 11:01:15 +0800

    完成添加和编辑课时

--- public/js/courselesson.js
+++ public/js/courselesson.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','bootstrap'],function($,template,util){
 	util.setMenu('/course/list');
 	//获取课程id
 	var csId=util.qs('cs_id');
@@ -11,6 +11,30 @@ define(['jquery','template','util'],function($,template,util){
     	success:function(data){
     		var html=template('lessonTpl',data.result);
     		$('#lessonInfo').html(html);
+    		//添加课时
+    		$('#addBtn').click(function(){
+    			var html=template('modalTpl',{operate:'添加课时'});
+    		    $('#modalInfo').html(html);
+    			$('#chapterModal').modal();
+    		});
+    		//处理编辑课时
+    		$('.editBtn').click(function(){
+    			//查询数据
+    			$.ajax({
+    				type:'get',
+    				url:'/api/course/chapter/edit',
+    				data:{ct_id:$(this).attr('data-ctId')},
+    				dataType:'json',
+    				success:function(data){
+    					//渲染模版
+    					data.result.operate='编辑课时';
+    					var html=template('modalTpl',data.result);
+    					$('#modalInfo').html(html);
+    					//显示弹窗
+    					$('#chapterModal').modal();
+    				}
+    			})
+    		})
     	}
     });
 });
